509a43a75286414bc0b5c627dddf220071b2dccb
akowalew <ram.techen@gmail.com>
2016-04-23 19:12:10 +0200

    Stworzenie klas Crossing oraz CrossSrvr Wrzucenie obsługi wywołania usługi 'add_crossing' do wnętrza CrossSrvr Zakodzenie, czy wprowadzany ID już czasem nie istnieje.

--- src/crossings/src/crossings_node.cpp
+++ src/crossings/src/crossings_node.cpp
@@ -2,28 +2,97 @@
 #include "crossings/add_crossing.h"
 #include "crossings/cross_msg.h"
 
-bool add(crossings::add_crossing::Request  &req,
-         crossings::add_crossing::Response &res)
+class Crossing
+{
+    private:
+        int16_t ID;
+        std::vector<int16_t>* neighbours;
+        std::vector<int16_t>* lengths;
+        
+    public:
+        Crossing(int16_t ID, std::vector<int16_t>* neighbours, std::vector<int16_t>* lengths)
+        {
+            this->neighbours = neighbours;
+            this->lengths = lengths;
+            this->ID = ID;
+        }
+
+        ~Crossing()
+        {
+            delete neighbours;
+            delete lengths;
+        }
+
+        int16_t getID() { return ID; }
+        std::vector<int16_t>& getNeighbours() { return *neighbours; }
+        std::vector<int16_t>& getLengths() { return *lengths; }
+};
+
+class CrossSrvr // Crossings Server
 {
 
-	// place some code to get data from MAP about new CROSSING
-	ROS_INFO("Get some data over \'add_crossing\' service :\n");
-	for(int i = 0 ; i < req.crossingsData.size() ; i++)
-	{
-		ROS_INFO("ID:%d NeighSize:%ld LengthSize:%ld", 
-			req.crossingsData[i].ID,
-			req.crossingsData[i].neighbours.size(),
-			req.crossingsData[i].lengths.size());
-	}
-  return true;
-}
+    CrossSrvr() {}
+    CrossSrvr(const CrossSrvr&) {}
+
+    std::vector<Crossing*> crossings;
+    
+    bool checkIfExistID(int16_t ID)
+    {
+        for(auto it = crossings.cbegin(); it != crossings.cend(); ++it)
+            if((*it)->getID() == ID)
+                return true;
+        return false;
+    }
+
+public:
+    void addCrossing(Crossing* newCross)
+    {
+        crossings.push_back(newCross);
+        // create new topic for cars
+    }
+
+    static CrossSrvr& getInstance()
+    {
+        static CrossSrvr thisInstance;
+        return thisInstance;
+    }
+
+    static bool addCross(crossings::add_crossing::Request  &req,
+         crossings::add_crossing::Response &res)
+    {
+	    // place some code to get data from MAP about new CROSSING
+        CrossSrvr& crossSrvr = CrossSrvr::getInstance();
+        int16_t tmpID;
+
+    	for(int i = 0 ; i < req.crossingsData.size() ; i++)
+    	{
+            tmpID = req.crossingsData[i].ID;        
+    
+            if(!crossSrvr.checkIfExistID(tmpID))
+            {
+                crossSrvr.addCrossing(new Crossing(
+                            tmpID,
+                            new std::vector<int16_t>(req.crossingsData[i].neighbours),
+                            new std::vector<int16_t>(req.crossingsData[i].lengths)));
+                ROS_INFO("Added new crossing. ID: %d. It\'s talking on \'cross_%d \' topic.", tmpID, tmpID);
+    	    }
+            else
+            {
+                ROS_INFO("Requested crossing with ID: %d arleady exist!", tmpID);
+            }
+        }
+      return true;
+    }
+};
+
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "crossings_server");
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("add_crossing_service", add);
+  ros::ServiceServer service = n.advertiseService("add_crossing_service", CrossSrvr::addCross);
   ROS_INFO("Ready to add new crossings.");
  
   ros::spin();
--- src/crossings/src/tmp_node.cpp
+++ src/crossings/src/tmp_node.cpp
@@ -6,15 +6,15 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "add_crossing_client");
 	ros::NodeHandle n;
+
 	ros::ServiceClient client = n.serviceClient<crossings::add_crossing>("add_crossing_service");
 	crossings::add_crossing srv;
 	
 	crossings::cross_msg msg;
-	//	srv.request.crossingsData = msg;
 	msg.ID = 10;
 	msg.neighbours.push_back(1122);
 	msg.lengths.push_back(10);
-
+        
 	srv.request.crossingsData.push_back(msg);
 	if(client.call(srv))
 	{
