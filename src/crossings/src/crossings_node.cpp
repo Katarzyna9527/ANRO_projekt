fc32b400b60d73cf3afa6c1a957bdea2bd262154
akowalew <ram.techen@gmail.com>
2016-04-23 17:37:40 +0200

    Przeprowadzenie testu wywołania usługi "add_crossing" Krótka poprawka w deklaracji add_crossing.srv Dodanie pliku tmp_node.cpp do CMakeLists

--- src/crossings/src/crossings_node.cpp
+++ src/crossings/src/crossings_node.cpp
@@ -7,7 +7,14 @@ bool add(crossings::add_crossing::Request  &req,
 {
 
 	// place some code to get data from MAP about new CROSSING
-
+	ROS_INFO("Get some data over \'add_crossing\' service :\n");
+	for(int i = 0 ; i < req.crossingsData.size() ; i++)
+	{
+		ROS_INFO("ID:%d NeighSize:%ld LengthSize:%ld", 
+			req.crossingsData[i].ID,
+			req.crossingsData[i].neighbours.size(),
+			req.crossingsData[i].lengths.size());
+	}
   return true;
 }
 
@@ -16,7 +23,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "crossings_server");
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("add_crossing", add);
+  ros::ServiceServer service = n.advertiseService("add_crossing_service", add);
   ROS_INFO("Ready to add new crossings.");
  
   ros::spin();
---
+++ src/crossings/src/tmp_node.cpp
@@ -0,0 +1,30 @@
+#include "ros/ros.h"
+#include "crossings/add_crossing.h"
+#include "crossings/cross_msg.h"
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "add_crossing_client");
+	ros::NodeHandle n;
+	ros::ServiceClient client = n.serviceClient<crossings::add_crossing>("add_crossing_service");
+	crossings::add_crossing srv;
+	
+	crossings::cross_msg msg;
+	//	srv.request.crossingsData = msg;
+	msg.ID = 10;
+	msg.neighbours.push_back(1122);
+	msg.lengths.push_back(10);
+
+	srv.request.crossingsData.push_back(msg);
+	if(client.call(srv))
+	{
+		ROS_INFO("Poszło!");
+	} 
+	else
+	{	
+		ROS_INFO("Nie poszło!");
+		return -1;
+	}
+
+	return 0;
+}
