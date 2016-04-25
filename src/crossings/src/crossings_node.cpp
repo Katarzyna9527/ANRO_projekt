0fa1b85b5c0cd25baf507bfd4a8c35ff38f731d1
akowalew <ram.techen@gmail.com>
2016-04-25 14:19:21 +0200

    Crossings: Działa, wstępnie (dla testu jednostkowego)

--- src/crossings/src/crossings_node.cpp
+++ src/crossings/src/crossings_node.cpp
@@ -9,6 +9,24 @@ map::cross_msg  crossCfg; // this crossing Configuration
 ros::Publisher crossPub;
 ros::Subscriber crossSub;
 ros::Subscriber lightsSub;
+std::vector<int16_t> myAvailDirs;
+
+std::vector<crossings::autocross_msg> waitingQueues[4];
+int carsThatCrossed[4] = {0, 0, 0, 0};
+bool isCrossOccupied = false;
+
+bool checkIfCanDrive(crossings::autocross_msg &response, int currCarPos);
+void pushAutoIntoCrossing(crossings::autocross_msg &response, int currCarPos);
+
+int calculateCarPosition(int previousCrossID)
+{
+    int currCarPos;
+    for(currCarPos = 0; currCarPos < 4; currCarPos++)
+        if(crossCfg.neighbours[currCarPos] == previousCrossID)
+            break;
+
+    return (currCarPos == 4 ? -1 : currCarPos);
+}
 
 bool isLightsPublishing = false;
 lights::LightState currLightsCfg ; // current lights config
@@ -21,14 +39,32 @@ void lightsCallback(const lights::LightState::ConstPtr& msg)
     ROS_INFO("\ts=[%d, %d, %d, %d]", msg->n.C, msg->e.C, msg->s.C, msg->w.C);
     ROS_INFO("\tw=[%d, %d, %d, %d]", msg->n.D, msg->e.D, msg->s.D, msg->w.D);
     currLightsCfg = lights::LightState(*msg);
+
+    crossings::autocross_msg waitingCar;
+    // sprawdzamy, czy czasem nie trzeba puścić następnej osoby z każdego kierunku
+    for(int i = 0 ; i < 4; i++)
+    {
+        if(waitingQueues[i].size() > 0)
+        {
+            waitingCar = waitingQueues[i].at(0);
+            if(checkIfCanDrive(waitingCar, i))
+                pushAutoIntoCrossing(waitingCar, i);
+
+        }
+    }
 }
 
-bool getAvailsDirections(crossings::autocross_msg &response)
+// Write available directions to response and return position of this car
+bool checkIfCanDrive(crossings::autocross_msg &response, int currCarPos)
 {
-    int currCarPos;
-    for(currCarPos = 0; currCarPos < 4; currCarPos++)
-        if(crossCfg.neighbours[currCarPos] == response.previousCrossID)
-            break;
+    if(isCrossOccupied)
+        return false;
+    ROS_INFO("CrossInfo: Cross is not occupied");
+
+    // musi byc pierwszy w kolejce, by mogl przejechac
+    if(waitingQueues[currCarPos].at(0).autoID != response.autoID)
+        return false;
+    ROS_INFO("CrossInfo: Auto with ID=%d is first at the queue on position=%d", response.autoID, currCarPos);
 
     lights::State dirStates;
     switch(currCarPos)
@@ -45,39 +81,89 @@ bool getAvailsDirections(crossings::autocross_msg &response)
         case 3:
             dirStates = currLightsCfg.w;
             break;
-        case 4:
+        default:
             return false;
     }
    
-    response.availableDirections.resize(4);
-    response.availableDirections[0] = dirStates.A;
-    response.availableDirections[1] = dirStates.B;
-    response.availableDirections[2] = dirStates.C;
-    response.availableDirections[3] = dirStates.D;
+    switch(response.direction)
+    {
+        case 0:
+            if(!dirStates.A)
+                return false;
+            break;
+        case 1:
+            if(!dirStates.B)
+                return false;
+            break;
+        case 2:
+            if(!dirStates.C)
+                return false;
+            break;
+        case 3:
+            if(!dirStates.D)
+                return false;
+            break;
+        default:
+            return false;
+    }
+
+    ROS_INFO("CrossInfo: Auto with ID=%d, could drive from position=%d", response.autoID, currCarPos);
     return true;
 }
 
-int previousCarsID[4] = {0, 0, 0, 0}; 
-bool isCrossOccupied = false;
+void pushAutoIntoCrossing(crossings::autocross_msg &response, int currCarPos)
+{
+    // usuwamy go z kolejki
+    waitingQueues[currCarPos].erase(waitingQueues[currCarPos].begin());
+                
+    // może jechać, wyślij potwierdzenie
+    isCrossOccupied = true;
+    response.length = crossCfg.lengths[response.direction];
+    response.nextCrossID = crossCfg.neighbours[response.direction];
+    response.previousAutoID = carsThatCrossed[response.direction];
+
+    crossPub.publish(response);
+    ROS_INFO("CrossInfo: I pushed auto with ID=%d into crossing!", response.autoID); 
+}
+
 void fromCarCallback(const crossings::autocross_msg::ConstPtr& msg)
 {
     if(msg->isMsgFromAuto == true)
     {
         ROS_INFO("I got msg from auto with ID:%d", msg->autoID);
 
-        if(msg->direction == -1) // send him all avail directions
+        if(msg->isCrossed)
+        {
+            ROS_INFO("\tCar has left the crossing.");
+            isCrossOccupied = false;
+            carsThatCrossed[msg->direction] = msg->autoID;
+        }
+        else if(msg->direction == -1) // send him all avail directions
         {
             ROS_INFO("\tI got msg with direction=-1");
             crossings::autocross_msg response = *msg;
             response.isMsgFromAuto = false;
             
-            if(!getAvailsDirections(response))
+            int currCarPos = calculateCarPosition(response.previousCrossID);
+            if(currCarPos == -1)
             {
                 ROS_INFO("\tBad previous cross ID=%d ! No response...", response.previousCrossID);
                 return;
             }
 
+            ROS_INFO("This car current position: %d", currCarPos);
+
+            response.availableDirections = myAvailDirs;
+    
+            if(waitingQueues[currCarPos].size() == 0)
+                response.previousAutoID = 0;
+            else
+                response.previousAutoID = waitingQueues[currCarPos].at(0).autoID;
+
+            waitingQueues[currCarPos].push_back(response);
             crossPub.publish(response);       
+
+            ROS_INFO("\tI send to auto msg with available directions.");
         }
         else if(msg->direction >= 0 && msg->direction <= 3)
         {
@@ -85,13 +171,24 @@ void fromCarCallback(const crossings::autocross_msg::ConstPtr& msg)
             crossings::autocross_msg response = *msg;
             response.isMsgFromAuto = false;
 
-            if(!getAvailsDirections(response))
+            int currCarPos = calculateCarPosition(response.previousCrossID);
+            if(currCarPos == -1)
             {
                 ROS_INFO("\tBad previous cross ID=%d ! No response...", response.previousCrossID);
                 return;
             }
+               
+            ROS_INFO("\tThis car current position : %d", currCarPos);
 
-
+            if(checkIfCanDrive(response, currCarPos))
+                pushAutoIntoCrossing(response, currCarPos);
+            else
+            {
+                ROS_INFO("\tCar can NOT drive! It has to wait.");
+                for(int i = 0 ; i < waitingQueues[currCarPos].size(); i++)
+                    if(waitingQueues[currCarPos].at(i).autoID == response.autoID)
+                        waitingQueues[currCarPos].at(i).direction = msg->direction;
+            }
         }
     }
 }
@@ -140,6 +237,11 @@ int main(int argc, char **argv)
             crossCfg.neighbours[0],crossCfg.neighbours[1],crossCfg.neighbours[2],crossCfg.neighbours[3]);
     ROS_INFO("\tLengths:    n=%d, e=%d, s=%d, w=%d",
             crossCfg.lengths[0], crossCfg.lengths[1], crossCfg.lengths[2], crossCfg.lengths[3]);
+
+    myAvailDirs = std::vector<int16_t>(4);
+    // ustaw, gdzie mozna skrecac, gdzie nie
+    for(int i = 0; i < 4; i++)
+        myAvailDirs[i] = (crossCfg.neighbours[i] == 0 ? 0 : 1);
 
     ss.str("");
     ss << "lights_" << crossCfg.ID;
--- src/crossings/src/crossings_test.cpp
+++ src/crossings/src/crossings_test.cpp
@@ -2,43 +2,102 @@
 #include "crossings/autocross_msg.h"
 #include <iostream>
 
+ros::Publisher testPub;
+ros::Subscriber testSub;
+int id;
+int stage = 0;
+int prev;
+int crossId;
+
+void testCallback(const crossings::autocross_msg::ConstPtr& msg)
+{
+    ros::NodeHandle n;
+    ROS_INFO("Get data from crossing");
+
+    if(msg->isMsgFromAuto)
+        return;
+
+    if(msg->autoID != id)
+        return;
+
+    if(msg->direction == -1) // we receive avails dirs
+    {
+        ROS_INFO("I receive available directions");
+        std::vector<int16_t> avDirs = msg->availableDirections;       
+        ROS_INFO("That is: [%d, %d, %d, %d]", avDirs[0], avDirs[1], avDirs[2], avDirs[3]);
+        ROS_INFO("The last car on my road is : %d", msg->previousAutoID);
+
+        int destDir;
+        std::cout << "Enter direction to drive to: ";
+        std::cin >> destDir;
+
+        crossings::autocross_msg response = *msg;
+        response.isMsgFromAuto = true;
+        response.direction = destDir;
+
+        testPub.publish(response);        
+    } else {
+        ROS_INFO("I receive acceptation from crossing");
+        ROS_INFO("Received data about next cross. ID=%d, len=%d", msg->nextCrossID, msg->length);
+        
+        std::string str;
+        std::cout << "Type enter to leave a crossing";
+        std::cin >> str;
+
+        crossings::autocross_msg response = *msg;
+        response.isMsgFromAuto = true;
+        response.isCrossed = true;
+        testPub.publish(response);
+
+        std::cout << "Type enter to drive to next crossing";
+        std::cin >> str;
+
+        response.isCrossed = false;
+        response.direction = -1;
+        response.previousCrossID = crossId;
+        
+        prev = crossId;
+        crossId = msg->nextCrossID;
+        
+        std::stringstream ss;
+        ss << "crossing_" << msg->nextCrossID;
+        testPub = n.advertise<crossings::autocross_msg>(ss.str().c_str(), 1000, true);
+        testSub = n.subscribe(ss.str().c_str(), 1000, testCallback);
+        
+        testPub.publish(response);
+    }
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "crossings_test_node");
     ros::NodeHandle n;
     
-    std::string str ;
-    std::cout << "Type topic to publish to: ";
-    std::cin >> str;
+    std::cout << "Type cross ID to drive : ";
+    std::cin >> crossId;
 
-    int id;
     std::cout << "Type this node ID: ";
     std::cin >> id;
 
-    int prev;
     std::cout << "Type previous cross ID: ";
     std::cin >> prev;
 
-    ros::Publisher testPub = n.advertise<crossings::autocross_msg>(str.c_str(), 1000, true);
-    ros::Rate loop_rate(1);
+    std::stringstream ss;
+    ss << "crossing_" << crossId;
 
-    short int count = 0;
-    while(ros::ok())
-    {
-        crossings::autocross_msg msg;
-        msg.autoID = id;
-        msg.isMsgFromAuto = true;
-        msg.direction = -1;
-        msg.previousCrossID = prev;
-        testPub.publish(msg);
+    testPub = n.advertise<crossings::autocross_msg>(ss.str().c_str(), 1000, true);
+    testSub = n.subscribe(ss.str().c_str(), 1000, testCallback);
+    
+    crossings::autocross_msg msg;
+    msg.autoID = id;
+    msg.isMsgFromAuto = true;
+    msg.direction = -1;
+    msg.isCrossed = false;
+    msg.previousCrossID = prev;
+    testPub.publish(msg);
 
-        ROS_INFO("Send data to topic %s. Count = %d", str.c_str(), count); 
+    ROS_INFO("Send data to topic %s with direction=-1.", ss.str().c_str()); 
 
-        ros::spinOnce();
-        loop_rate.sleep();
-        ++count ;
-    }
- 
-
+    ros::spin();
     return 0;
 }
