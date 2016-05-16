1bc2bfdabddb9d37cee26f1240962ba0247eb565
pastorofmuppetz <pankiewiczkuba@gmail.com>
2016-05-17 00:34:08 +0200

    Światła - bez nawrotów i z lepszymi zakrętami

--- src/lights/src/lights_node.cpp
+++ src/lights/src/lights_node.cpp
@@ -13,156 +13,307 @@ using namespace std;
 
 #include <string>
 
-class DirectionAllower{									//klasa odpowiadająca za zmianę stanu świateł we wszystkich kierunkach na skrzyżowaniu
-	private:
-		bool directionMatrix [4][4];					//macierze odpowiadająca przejazdowi z każdego kierunku do innego kierunku (albo do siebie samego)
-	public:
-		bool shift();									//przesuwanie wierszy macierzy -> potrzebne do zmiany stanu świateł
-		bool shiftRight(int i);							//przesuwanie w prawo wiersza 'i'
-		bool shiftLeft (int i);							//jw. tylko w lewo
-		vector <bool> takeDirections(int direction);	//pobranie wektora dozwolonych kierunków przejazdu, dla określonego kierunku (wektor kolumnowy)
-		DirectionAllower();								//konstruktor
-		~DirectionAllower();
-		void show();									//klasa pomocnicza, pokazująca, czy wszystko działa OK
+
+::map::map_config map_data;		//structure with map configure
+
+//function to create LightState structures from bool-information
+lights::LightState make_lightState(bool a1, bool b1, bool c1, bool d1, 			//from north to other directions (n,e,s,w)
+				   bool a2, bool b2, bool c2, bool d2, 			//from east to other directions (n,e,s,w)
+				   bool a3, bool b3, bool c3, bool d3, 			//from south to other directions (n,e,s,w)
+				   bool a4, bool b4, bool c4, bool d4){			//from west to other directions (n,e,s,w)
+
+    lights::State lightsFor1;
+    lightsFor1.A = a1;
+    lightsFor1.B = b1;
+    lightsFor1.C = c1;
+    lightsFor1.D = d1;
+
+    lights::State lightsFor2;
+    lightsFor2.A = a2;
+    lightsFor2.B = b2;
+    lightsFor2.C = c2;
+    lightsFor2.D = d2;
+
+    lights::State lightsFor3;
+    lightsFor3.A = a3;
+    lightsFor3.B = b3;
+    lightsFor3.C = c3;
+    lightsFor3.D = d3;
+
+    lights::State lightsFor4;
+    lightsFor4.A = a4;
+    lightsFor4.B = b4;
+    lightsFor4.C = c4;
+    lightsFor4.D = d4;
+
+    lights::LightState lightsForAll;
+    lightsForAll.n = lightsFor1;
+    lightsForAll.e = lightsFor2;
+    lightsForAll.s = lightsFor3;
+    lightsForAll.w = lightsFor4;
+
+    return lightsForAll;
+}
+
+//virtual class
+class Allower{			//lights - node
+
+protected:
+    int which_state;				//current state
+    int max_state;				//how many states are able
+    int id;					//id of crossing
+
+public:
+    virtual void change_state()=0;
+    virtual lights::LightState create_message()=0;
+
 };
-																														//SKĄD
-DirectionAllower::DirectionAllower(){					//konstruktor												 D	 N E S W
-	bool tab [4][4] = { {1,0,0,0},						//tworzymy macierz zgodnie z takim oto założeniem: 		   	 O N X X X X
-						{1,0,0,0},						//															 K E X X X X
-						{0,0,1,0},						//															 Ą S X X X X
-						{0,0,1,0},};					//                     										 D W X X X X
-	for(int i = 0; i < 4; i++)							//przypisujemy tę macierz do macierzy w klasie
-		for(int j = 0; j < 4; j++)
-			directionMatrix[i][j] = tab[i][j];
 
-}
+//crossing with 4 roads
+class Allower4: public Allower{
 
-DirectionAllower::~DirectionAllower(){					//destruktor
+private:
+    lights::LightState allower_states[4];	//all states that may occure
 
-}
+public:
 
-bool DirectionAllower::shiftRight(int i){				//przesuwanie w prawo wiersza 'i' macierzy
-	int temp;
-	temp = directionMatrix[i][3];
-	for(int j = 3; j > 0; j--)
-		directionMatrix[i][j] = directionMatrix[i][j-1];
-	directionMatrix[i][0] = temp;
-	return true;
-}
+    Allower4(int index=0,int starting_state=1){
+        max_state = 4;
+        which_state=starting_state%max_state;
+        id=index;
+        allower_states[0]= make_lightState(0,0,1,1,  0,0,0,0,  1,1,0,0,  0,0,0,0);
+        allower_states[1]= make_lightState(0,0,0,0,  1,0,0,1,  0,0,0,0,  0,1,1,0);
+        allower_states[2]= make_lightState(0,1,0,0,  0,0,0,0,  0,0,0,1,  0,0,0,0);
+        allower_states[3]= make_lightState(0,0,0,0,  0,0,1,0,  0,0,0,0,  1,0,0,0);
+    }
 
-bool DirectionAllower::shiftLeft(int i){				//przesuwanie w lewo wiersza 'i' macierzy
-	int temp;
-	temp = directionMatrix[i][0];
-	for(int j = 0; j < 3; j++)
-		directionMatrix[i][j] = directionMatrix[i][j+1];
-	directionMatrix[i][3] = temp;
-	return true;
-}
+     lights::LightState create_message(){
+        return allower_states[which_state];
+     }
 
-bool DirectionAllower::shift(){							//przesuwanie, odpowiadające zmianie stanu naszych świateł
-	this->shiftLeft(0);
-	this->shiftRight(1);
-	this->shiftLeft(2);
-	this->shiftRight(3);
-	return true;
-}
+     void change_state(){
+        which_state++;
+        which_state = which_state%max_state;
+    }
+};
 
-vector <bool> DirectionAllower::takeDirections(int direction){			//pobranie kierunków do jazdy, dla odpowiedniego wlotu skrzyżowania
-	vector <bool> column;
-	for(int i = 0; i < 4; i++)
-		column.push_back(directionMatrix[i][direction]);				//do wektora wstawiamy każdy element z kolumny o numerze "direction" (N=0,E=1,S=2,W=3)
-	return column;
-}
+//crossing with 3 roads
+class Allower3: public Allower{
 
-void DirectionAllower::show(){
-	for(int i = 0; i < 4; i++){
-		for(int j = 0; j < 4; j++)
-			cout<<directionMatrix[i][j]<<' ';
-		cout<<'\n';
-	}
-	cout<<'\n';
-}
+private:
+    lights::LightState allower_states[3];
+    int index_of_empty_road;
+
+public:
+
+    Allower3(int index=0, int starting_state=0){
+        max_state = 3;
+        which_state=starting_state % max_state;
+        id=index;
+        check_which_road_empty();
+        switch(index_of_empty_road){
+
+    case 0:
+        {
+            allower_states[0]= make_lightState(0,0,0,0,  0,0,0,1,  0,0,0,0,  0,1,1,0);
+            allower_states[1]= make_lightState(0,0,0,0,  0,0,0,0,  0,1,0,1,  0,0,0,0);
+            allower_states[2]= make_lightState(0,0,0,0,  0,0,1,1,  0,0,0,0,  0,0,0,0);
+            break;
+         }
+    case 1:
+        {
+            allower_states[0]= make_lightState(0,0,1,1,  0,0,0,0,  1,0,0,0,  0,0,0,0);
+            allower_states[1]= make_lightState(0,0,0,0,  0,0,0,0,  0,0,0,0,  1,0,1,0);
+            allower_states[2]= make_lightState(0,0,0,0,  0,0,0,0,  1,0,0,0,  0,0,0,0);
+            break;
+         }
+    case 2:
+        {
+            allower_states[0]= make_lightState(0,0,0,0,  1,0,0,1,  0,0,0,0,  0,1,0,0);
+            allower_states[1]= make_lightState(0,1,0,1,  0,0,0,0,  0,0,0,0,  0,0,0,0);
+            allower_states[2]= make_lightState(0,0,0,0,  0,0,0,0,  0,0,0,0,  1,1,0,0);
+            break;
+        }
+    case 3:
+        {
+            allower_states[0]= make_lightState(0,0,1,0,  0,0,0,0,  1,1,0,0,  0,0,0,0);
+            allower_states[1]= make_lightState(0,0,0,0,  1,0,1,0,  0,0,0,0,  0,0,0,0);
+            allower_states[2]= make_lightState(0,1,1,0,  0,0,0,0,  0,0,0,0,  0,0,0,0);
+            break;
+        }
+        }
+
+    }
+
+    void check_which_road_empty(){
+        for(int i =0; i< 4; ++i){
+            if(map_data.response.crossings[id-1].neighbours[i]<=0){
+                index_of_empty_road=i;
+                break;
+            }
+        }
+    }
+
+    lights::LightState create_message(){
+	return allower_states[which_state];
+    }
+
+     void change_state(){
+        which_state++;
+        which_state = which_state%max_state;
+    }
+};
+
+//crossing with 2 roads
+class Allower2 : public Allower{
+
+private:
+    lights::LightState state;
+    int index_of_empty_road1;
+    int index_of_empty_road2;
+
+public:
+
+    Allower2(int index=0, int starting_state=0){
+        id=index;
+        check_which_road_empty();	//checking which roads arent empty
+		
+        if((index_of_empty_road1 + index_of_empty_road2) % 2 == 0)
+		cout<<"HERE I HAVE CROSSING THAT IS NOT A CROSSING :)"<<endl;
+
+        else if(index_of_empty_road1 + index_of_empty_road2 == 1)
+		state=make_lightState(0,1,0,0,  1,0,0,0,  0,0,0,0,  0,0,0,0);
+
+        else if(index_of_empty_road1 + index_of_empty_road2 == 5)
+		state=make_lightState(0,0,0,0,  0,0,0,0,  0,0,0,1,  0,0,1,0);
+
+        else if(index_of_empty_road1 + index_of_empty_road2 == 3){
+
+            if(index_of_empty_road1 == 0 || index_of_empty_road2 ==0)
+                state=make_lightState(0,0,0,1,  0,0,0,0,  0,0,0,0,  1,0,0,0);
+
+            else
+                state=make_lightState(0,0,0,0,  0,0,1,0,  0,1,0,0,  0,0,0,0);
+        }
+    }
+
+    void check_which_road_empty(){
+        for(int i =0; i< 4; ++i){
+            if(map_data.response.crossings[id-1].neighbours[i]>0){
+                index_of_empty_road1=i;
+                i++;
+                for(int j=i; j<4; ++j){
+                    if(map_data.response.crossings[id-1].neighbours[j]>0){
+                        index_of_empty_road2=j;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+     lights::LightState create_message(){
+        return state;
+     }
+
+     void change_state(){
+        //nothing to do
+    }
+};
+
+class LightsOnCrossing{
+
+    int id;
+    Allower* allower;
+    ros::NodeHandle lightsNode;
+    ros::Publisher lightsPub;
+
+public:
+    LightsOnCrossing(int number_of_crossing=0){
+        id = number_of_crossing;
+
+         int nRodes=0;		//how many roads
+        for(int i =0; i<4; ++i){
+            if(map_data.response.crossings[id-1].neighbours[i]>0)nRodes++; 
+        }
+  
+
+        if(nRodes==4)
+            allower = new Allower4(number_of_crossing, number_of_crossing);
+
+        else if(nRodes==3)
+            allower = new Allower3(number_of_crossing, number_of_crossing);
+        else if(nRodes==2)
+            allower = new Allower2(number_of_crossing, number_of_crossing);
+
+
+        stringstream ss;
+        ss<<id;
+        
+        string topicName="lights_";
+        string number=ss.str();
+        topicName=topicName+number;
+            lightsPub = lightsNode.advertise<lights::LightState>(topicName, 1000);
+       // cout<<"\n"<<"Lights for crossing " + number + " created";
+    }
+
+    void next_state(){
+        allower->change_state();
+    }
+
+    void send_msg(){
+
+        lightsPub.publish(allower->create_message());
+    }
+};
 
 ::map::map_config getMapConfiguration(){								//POBIERANIE INFORMACJI Z MAPY
 
     ::map::map_config mapConfiguration;
-    ros::NodeHandle mapNode;											
+    ros::NodeHandle mapNode;
 
     ros::ServiceClient lightsClient = mapNode.serviceClient<::map::map_config>("get_map_config");
     mapConfiguration.request.req = 1;
+    ROS_INFO("Waiting for map...");
     while(1){
-	if(lightsClient.call(mapConfiguration)){
-		break;
-	}
-	else{
-		ROS_INFO("Failed to call service get_map_config");
-	}
+        if(lightsClient.call(mapConfiguration))
+            break;
     }
-
+    ROS_INFO("Map received!");
     return mapConfiguration;
 }
 
-int main(int argc, char **argv)											//główny program
+
+
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "lights");
 
-    ::map::map_config map = getMapConfiguration();
-	cout<<"\n"<<"Map configuration received";
+    map_data = getMapConfiguration();
+        cout<<"/n"<<"Map configuration received"<<endl;
 
-    int nNodes=map.response.number_of_crossings;						//number of nodes
+    int nNodes=map_data.response.number_of_crossings;
 
-    DirectionAllower *allower [nNodes];									//tablica przełączników stanu
-    lights::LightState lightStates [nNodes];							//struktury do wysyłania informacji skrzyżowaniu
+    LightsOnCrossing *lights_on_crossings[nNodes];
 
-	for(int x = 0; x < nNodes; x++){									//pętla, w której tworzymy nNodes sygnalizatorów, a ponadto przesuwamy je w zależności
-            allower[x] = new DirectionAllower();
-            for(int y = x%4; y > 0; y--)
-                allower[x]->shift();
-        }
-    lights::State states [4];											//wszystkie sygnalizatory aktualizujemy, przechodząc do kolejnego stanu
-
-    ros::NodeHandle lightsNodes[nNodes];
-    ros::Publisher lightsPubs[nNodes];
-
-    for(int i =0;i<nNodes;++i){
-	stringstream ss;
-	ss<<map.response.crossings[i].ID;
-	string topicName="lights_";
-	string number=ss.str();
-	topicName=topicName+number; 
-        lightsPubs[i] = lightsNodes[i].advertise<lights::LightState>(topicName, 1000);
-	cout<<"\n"<<"Lights for crossing " + number + " created";
+    for(int i =0;i<nNodes; ++i){
+        lights_on_crossings[i] = new LightsOnCrossing(map_data.response.crossings[i].ID);
     }
+
 	cout<<"\n"<<"Lights done";
 	ros::Rate loop_rate(0.25);
-	
+
     while(ros::ok()){
-			for(int x = 0; x < nNodes; x++){
+            for(int i =0; i < nNodes; ++i){
 
-				allower[x]->shift();
+                lights_on_crossings[i]->send_msg();
+                lights_on_crossings[i]->next_state();
+            }
 
-				for(int i  = 0; i < 4; i++){
-
-					vector <bool> directions = allower[x]->takeDirections(i);
-					states[i].A = directions[0];
-					if(map.response.crossings[x].neighbours[0]<=0)states[i].A=false;
-					states[i].B = directions[1];
-					if(map.response.crossings[x].neighbours[1]<=0)states[i].B=false;
-					states[i].C = directions[2];
-					if(map.response.crossings[x].neighbours[2]<=0)states[i].C=false;
-					states[i].D = directions[3];
-					if(map.response.crossings[x].neighbours[3]<=0)states[i].D=false;
-				}
-				lightStates[x].n = states[0];
-				lightStates[x].e = states[1];
-				lightStates[x].s = states[2];
-				lightStates[x].w = states[3];
-				lightsPubs[x].publish(lightStates[x]);
-			}
-			ROS_INFO("Publishing finished");
-			
-			ros::spinOnce();
-			loop_rate.sleep();
+        ros::spinOnce();
+        loop_rate.sleep();
     }
-	return 0;
+
+    return 0;
 }
